23fb09ba6c5c0bdd6a26e225df79c1a80901095f
xing gao <2416457672@qq.com>
2022-06-04 00:39:54 +0800

    对接django

---
+++ src/component/content/calculator/operationButton.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import ACTIONS from '../../../redux/actions';
+
+class OperationButton extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <button
+                onClick={() => {
+                    this.props.choose_operation(this.props.operation);
+                }}
+                >
+                    {this.props.operation}
+                </button>
+        );
+    }
+}
+
+const mapDispatchToProps = {
+    choose_operation: operation => {
+        return {
+            type: ACTIONS.CHOOSE_OPERACTION,
+            operation: operation,
+        }
+    }
+}
+ 
+export default connect(null, mapDispatchToProps)(OperationButton);
---
+++ src/redux/actions.js
@@ -0,0 +1,9 @@
+const ACTIONS = {
+    ADD_DIGIT: "add-digit",
+    DELETE_DIGIT: "delete-digit",
+    CHOOSE_OPERACTION: "chosse-optiation",
+    CLEAR: "clear",
+    EVALUATE: "evaluate"
+};
+
+export default ACTIONS;
